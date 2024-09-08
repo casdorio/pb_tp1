550530bf9b080d6f9b3315490ea50768557e1d9a
casdorio <casdorio@gmail.com>
2024-09-08 10:59:54 -0300

    step 1

---
+++ src/main/java/com/carlos/adriano/infnet/pb_tp1/PbTp1Application.java
@@ -0,0 +1,13 @@
+package com.carlos.adriano.infnet.pb_tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbTp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/adriano/infnet/pb_tp1/PbTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.adriano.infnet.pb_tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
